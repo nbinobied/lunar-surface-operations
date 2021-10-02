591fb96e97969fca35c3252149f68257e66d2cc4
Nasser BinObied <19620026+nbinobied@users.noreply.github.com>
2021-10-02 21:17:36 +0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import "firebase/compat/database";
 import { getStorage } from "firebase/storage";
 
-const config = {
-  apiKey: "AIzaSyCQAycBu2pZsw_4JQLzRL_aESIis5csHnc",
-  authDomain: "lunar-surface-operations.firebaseapp.com",
-  databaseURL: "https://lunar-surface-operations-default-rtdb.europe-west1.firebasedatabase.app",
-  storageBucket: 'gs://lunar-surface-operations.appspot.com'
-};
+
 
 export const app =  firebase.initializeApp(config);
 export const auth = firebase.auth;
